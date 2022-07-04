9f7b6fc9f94683dc97bd3dc91e8944daf3200815
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-07-04 17:37:38 +0500

    For08

--- For08.py
+++ For08.py
@@ -7,7 +7,7 @@ def main(N):
         float: return  answer
     """
     sum = 0
-    for i in range(0,N):
-        sum += i/N
+    for i in range(1,N):
+        sum += 1+i/N
     return sum
-print(main(5))
+print(main(6))
