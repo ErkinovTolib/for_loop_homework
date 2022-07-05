c85c1d81092337b1852e9883506aefed356d0022
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-07-05 22:59:57 +0500

    For09

--- For09.py
+++ For09.py
@@ -1,9 +1,17 @@
 def main(price):
     """
-    The price of a kilogram of sweets is given. Return the price of a dessert from one to ten kg in the form of a list.
+    The price of a kilogram of sweets is given. 
+    Return the price of a dessert from one to ten kg in the form of a list.
     Args:
         price: int
     Returns:
         list: return  answer
     """
-    return
+    x = []
+    sum = price
+    for i in range(1,11):
+        x.append(price)
+        price += sum
+    return x
+print(main(1000))
+        
